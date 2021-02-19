e0306d569760e4e258a439a1488949772bac9977
ivanstepanovmipt <57829855+ivanstepanovmipt@users.noreply.github.com>
2021-02-19 22:35:54 +0500

    Update projectad.py

--- projectad.py
+++ projectad.py
@@ -1,3 +1,6 @@
+import logging
+from socket import *
+logging.basicConfig(level=logging.DEBUG)
 s = input()
 lst = s.split()
 serverName = lst[0]
@@ -14,9 +17,43 @@ while i < len(lst):
     if lst[i].find('f') != -1:
         read = lst[len(lst) - 1]
     i = i + 1
-print(protocol)
-print(read)
-print(client)
+if protocol == '-t':
+    if client == 1:
+        serverSocket = socket(AF_INET, SOCK_DGRAM)
+        serverSocket.bind(('', int(serverPort)))
+        print("The server is ready to receive")
+        while 1:
+            message, clientAddress = serverSocket.recvfrom(2048)
+            modifiedMessage = message.upper()
+            serverSocket.sendto(modifiedMessage, clientAddress)
+    else :
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        message = input('Input lowercase sentence:')
+        clientSocket.sendto(message,(serverName,
+        serverPort))
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        print(modifiedMessage)
+        clientSocket.close()
+else:
+    if client == 1:
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((serverName,serverPort))
+        sentence = input('Input lowercase sentence:')
+        clientSocket.send(sentence)
+        modifiedSentence = clientSocket.recv(1024)
+        print('From Server:', modifiedSentence)
+        clientSocket.close()
+    else:
+        serverSocket = socket(AF_INET,SOCK_STREAM)
+        serverSocket.bind(('',serverPort))
+        serverSocket.listen(1)
+        print('The server is ready to receive')
+        while 1:
+            connectionSocket, addr = serverSocket.accept()
+            sentence = connectionSocket.recv(1024)
+            capitalizedSentence = sentence.upper()
+            connectionSocket.send(capitalizedSentence)
+            connectionSocket.close()
         
         
     
