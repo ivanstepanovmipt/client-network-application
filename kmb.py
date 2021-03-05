605c7e9f03bf0d5852a4c6fdfcd0d7c04d20b955
ivanstepanovmipt <57829855+ivanstepanovmipt@users.noreply.github.com>
2021-03-05 23:53:46 +0100

    Update kmb.py

--- kmb.py
+++ kmb.py
@@ -2,19 +2,27 @@ import logging
 from socket import *
 import argparse
 import sys
-logging.basicConfig(level=logging.DEBUG)
 parser = argparse.ArgumentParser()
 
 
-def PrintInFile(name_file, log):
-    name_file = read + ".txt"
-    file = open("name_file", "w")
-    file.write(str(logging.info('opening a socket')))
-    file.close()
+def Logging(name_file, logging_mode, log):
+    if logging_mode == '-o':
+        logging.basicConfig(level=logging.DEBUG)
+        logging.info(str(log))
+    else:
+        logging.basicConfig(filename=name_file, level=logging.INFO)
+        logging.basicConfig(
+            filename=name_file, 
+            filemode='w', 
+            format='%(name)s - %(levelname)s - %(message)s'
+        )
+        logging.info(str(log))
+        print(1)
+		
 
 def create_parser_argv2():
-	parser.add_argument('-f', default = True) 
-	return parser
+    parser.add_argument('-f', default = True) 
+    return parser
 
 
 parser.add_argument("sN", type = str)
@@ -34,16 +42,19 @@ client = '-c'
 protocol = '-t'
 read = '-o'
 if args.s:
-	client = '-s'
+    client = '-s'
+if args.u and args.t:
+    logging.info('error')
+    sys.exit()
 if args.u:
-	protocol = '-u'
+    protocol = '-u'
 file_ = str(namespace.f)
 print(file_)
 if file_ == "True":
-	read = '-o'
+    read = '-o'
 if file_ != "True":
-	read = '-f'
-	_file = namespace.f
+    read = '-f'
+    _file = namespace.f
 print(serverName)
 print(serverPort)
 print(client)
@@ -56,84 +67,48 @@ print(file_)
 if protocol == '-u':
     if client == '-s':
         serverSocket = socket(AF_INET, SOCK_DGRAM)  # создаём сокет клиента
-        ##if read == '-o':
-        logging.info('opening a socket')
-        ##else:
-            ##PrintInFile(read, 'opening a socket')
+        Logging(file_, read, 'opening a socket with a protocol u')
         serverSocket.bind(('', int(serverPort)))   # связываем порт
         while 1:
             message, clientAddress = serverSocket.recvfrom(2048)
             message = str(clientAddress[0]) + ':' + str(clientAddress[1])
             serverSocket.sendto(message.encode(), clientAddress)  # отправляет 
-            # ответ
-            ##if read == '-o':
-            logging.info('Sending a message')
-            ##else:
-               ## PrintInFile(read, 'Sending a message')
+            Logging(file_, read, 'sending a message to a client')
     else:
         clientSocket = socket(AF_INET, SOCK_DGRAM)  # создаём сокет клиента
-        ##if read == '-o':
-        logging.info('opening a socket')
-        ##else:
-         ##   PrintInFile(read, 'opening a socket')
+        Logging(file_, read, 'opening a socket with a protocol u')
         message = input('Input lowercase sentence:')
         clientSocket.sendto(message.encode(), (serverName, int(serverPort))) 
-        ##if read == '-o':
-        logging.info('Sending a message')
-        ##else:
-           ## PrintInFile(read, 'Sending a message')
-        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)  # ждёт 
-        # ответ сервера;
-        ##if read == '-o':
-        logging.info('Receiving a message')
-        ##else:
-            ##PrintInFile(read, 'Receiving a message')
+        Logging(file_, read, 'sending a message to a server')
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)  # ждёт
+        Logging(file_, read, 'Receiving a message') 
         print(modifiedMessage.decode())  # выводит полученный ответ
         clientSocket.close()  # завершает свою работу.
-        ##if read == '-o':
-        logging.info('Closing the socket')
-        ##else:
-            ##PrintInFile(read, 'Closing the socket')
+        Logging(file_, read, 'Closing the socket')
 else:
     if client != '-s':
         clientSocket = socket(AF_INET, SOCK_STREAM)  # создаём сокет клиента
-        ##if read == '-o':
-        logging.info('opening a socket')
+        Logging(file_, read, 'opening a socket with a protocol t')
         ##else:
         ## PrintInFile(read, 'opening a socket')
         clientSocket.connect((serverName, int(serverPort)))
         sentence = input('Input lowercase sentence:')
         clientSocket.send(sentence.encode())  # посылает запрос серверу
-        ##if read == '-o':
-        logging.info('Sending a message')
-        ##else:
-            ##PrintInFile(read, 'Sending a message')
+        Logging(file_, read, 'sending a message to a server')
         modifiedSentence = clientSocket.recv(1024)  # ждёт ответ сервера;
-        ##if read == '-o':
-        logging.info('Receiving a message')
-        ##else:
-           ## PrintInFile(read, 'Receiving a message')
+        Logging(file_, read, 'Receiving a message')
         print('From Server:', modifiedSentence.decode()) 
         clientSocket.close()  # завершает свою работу.
-        ##if read == '-o':
-        logging.info('Closing the socket')
-        ##else:
-           ## PrintInFile(read, 'Closing the socket')
+        Logging(file_, read, 'Closing the socket')
     else:
         serverSocket = socket(AF_INET, SOCK_STREAM)  # создаём сокет клиента
-        ##if read == '-o':
-        logging.info('opening a socket')
-        ##else:
-            ##PrintInFile(read, 'opening a socket')
+        Logging(file_, read, 'opening a socket with a protocol t')
         serverSocket.bind(('', int(serverPort)))
         serverSocket.listen(1)
         while 1:
             connectionSocket, addr = serverSocket.accept()
             sentence = connectionSocket.recv(1024)
             capitalizedSentence = str(addr[0]) + ':' + str(addr[1])
-            connectionSocket.send(capitalizedSentence.encode())
-            ##if read == '-o':
-            logging.info('Sending a message')
-            ##else:
-               ## PrintInFile(read, 'Sending a message')
+            connectionSocket.send(capitalizedSentence.encode()) 
+            Logging(file_, read, 'sending a message to a client')
             connectionSocket.close()
