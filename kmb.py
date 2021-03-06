6e70ec8183c3879f36871e447e3a7307a18568e6
ivanstepanovmipt <57829855+ivanstepanovmipt@users.noreply.github.com>
2021-03-06 13:08:30 +0100

    Update kmb.py

--- kmb.py
+++ kmb.py
@@ -1,12 +1,12 @@
 import logging
-from socket import *
+from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM
 import argparse
 import sys
-from __future__ import print_function
 parser = argparse.ArgumentParser()
 
 
 def Logging(name_file, logging_mode, log):
+    """Logging to file or command line."""
     if logging_mode == '-o':
         logging.basicConfig(level=logging.DEBUG)
         logging.info(str(log))
@@ -21,60 +21,65 @@ def Logging(name_file, logging_mode, log):
 
 
 def create_parser_argv2():
+    """nothing."""
     parser.add_argument('-f', default=True)
     return parser
 
 
-def UDP_Server(serverName, serverPort, read):
-    serverSocket = socket(AF_INET, SOCK_DGRAM)
-    Logging(file_, read, 'opening a socket with a protocol u')
-    serverSocket.bind(('', int(serverPort)))
+def UDP_Server(Portserver, r):
+    """Starting the server in UDP mode."""
+    server_socket = socket(AF_INET, SOCK_DGRAM)
+    Logging(file_, r, 'opening a socket with a protocol u')
+    server_socket.bind(('', int(Portserver)))
     while 1:
-        message, clientAddress = serverSocket.recvfrom(2048)
-        message = str(clientAddress[0]) + ':' + str(clientAddress[1])
-        serverSocket.sendto(message.encode(), clientAddress)
-        Logging(file_, read, 'sending a message to a client')
+        message, client_address = server_socket.recvfrom(2048)
+        message = str(client_address[0]) + ':' + str(client_address[1])
+        server_socket.sendto(message.encode(), client_address)
+        Logging(file_, r, 'sending a message to a client')
 
 
-def UDP_Client(serverName, serverPort, read):
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    Logging(file_, read, 'opening a socket with a protocol u')
+def UDP_Client(Nameserver, Portserver, r):
+    """Starting the client in UDP mode."""
+    client_socket = socket(AF_INET, SOCK_DGRAM)
+    Logging(file_, r, 'opening a socket with a protocol u')
     message = 'hello'
-    clientSocket.sendto(message.encode(), (serverName, int(serverPort)))
-    Logging(file_, read, 'sending a message to a server')
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-    Logging(file_, read, 'Receiving a message')
-    print(modifiedMessage.decode())
-    clientSocket.close()
-    Logging(file_, read, 'Closing the socket')
+    client_socket.sendto(message.encode(), (Nameserver, int(Portserver)))
+    Logging(file_, r, 'sending a message to a server')
+    modified_message, server_address = client_socket.recvfrom(2048)
+    Logging(file_, r, 'Receiving a message')
+    print(modified_message.decode())
+    client_socket.close()
+    Logging(file_, r, 'Closing the socket')
 
 
-def TSP_Client(serverName, serverPort, read):
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    Logging(file_, read, 'opening a socket with a protocol t')
-    clientSocket.connect((serverName, int(serverPort)))
+def TSP_Client(Nameserver, Portserver, r):
+    """Starting the client in TSP mode."""
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    Logging(file_, r, 'opening a socket with a protocol t')
+    client_socket.connect((Nameserver, int(Portserver)))
     sentence = 'hello'
-    clientSocket.send(sentence.encode())
-    Logging(file_, read, 'sending a message to a server')
-    modifiedSentence = clientSocket.recv(1024)
-    Logging(file_, read, 'Receiving a message')
-    print('From Server:', modifiedSentence.decode())
-    clientSocket.close()
-    Logging(file_, read, 'Closing the socket')
+    client_socket.send(sentence.encode())
+    Logging(file_, r, 'sending a message to a server')
+    modified_sentence = client_socket.recv(1024)
+    Logging(file_, r, 'Receiving a message')
+    print('From Server:', modified_sentence.decode())
+    client_socket.close()
+    Logging(file_, r, 'Closing the socket')
 
 
-def TSP_Server(serverName, serverPort, read):
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    Logging(file_, read, 'opening a socket with a protocol t')
-    serverSocket.bind(('', int(serverPort)))
-    serverSocket.listen(1)
+def TSP_Server(Portserver, r):
+    """Starting the server in TSP mode."""
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    Logging(file_, r, 'opening a socket with a protocol t')
+    server_socket.bind(('', int(Portserver)))
+    server_socket.listen(1)
     while 1:
-        connectionSocket, addr = serverSocket.accept()
-        sentence = connectionSocket.recv(1024)
-        capitalizedSentence = str(addr[0]) + ':' + str(addr[1])
-        connectionSocket.send(capitalizedSentence.encode())
-        Logging(file_, read, 'sending a message to a client')
-        connectionSocket.close()
+        connection_socket, addr = server_socket.accept()
+        sentence = connection_socket.recv(1024)
+        capitalized_sentence = str(addr[0]) + ':' + str(addr[1])
+        connection_socket.send(capitalized_sentence.encode())
+        Logging(file_, r, 'sending a message to a client')
+        connection_socket.close()
 
 
 parser.add_argument("sN", type=str)
@@ -96,12 +101,12 @@ read = '-o'
 if args.s:
     client = '-s'
 if args.u and args.t:
+    logging.basicConfig(level=logging.DEBUG)
     logging.info('error')
     sys.exit()
 if args.u:
     protocol = '-u'
 file_ = str(namespace.f)
-print(file_)
 if file_ == "True":
     read = '-o'
 if file_ != "True":
@@ -109,12 +114,11 @@ if file_ != "True":
     file_ = namespace.f
 if protocol == '-u':
     if client == '-s':
-        UDP_Server(serverName, serverPort, read)
+        UDP_Server(serverPort, read)
     else:
         UDP_Client(serverName, serverPort, read)
 else:
     if client != '-s':
         TSP_Client(serverName, serverPort, read)
     else:
-        TSP_Server(serverName, serverPort, read)
-
+        TSP_Server(serverPort, read)
