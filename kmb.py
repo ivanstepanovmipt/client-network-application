a92bed6842c762e2c564da37ae7cd01e959de418
ivanstepanovmipt <57829855+ivanstepanovmipt@users.noreply.github.com>
2021-04-05 20:41:41 +0200

    Update kmb.py

--- kmb.py
+++ kmb.py
@@ -8,7 +8,6 @@ import argparse
 import sys
 # pylint: disable=E1101
 
-
 def logg(name_file, logging_mode, log):
     """Logging to file or command line."""
     if logging_mode == '-o':
@@ -24,59 +23,59 @@ def logg(name_file, logging_mode, log):
 
 def create_parser_argv2():
     """nothing."""
-    PARSER.add_argument('-f', default=True)
-    return PARSER
+    parser.add_argument('-f', default=True)
+    return parser
 
 
 def udp_server(port_server, r):
     """Starting the server in UDP mode."""
     server_socket = socket(AF_INET, SOCK_DGRAM)
-    logg(FILE_, r, 'opening a socket with a protocol u')
+    logg(file_, r, 'opening a socket with a protocol u')
     server_socket.bind(('', int(port_server)))
     while 1:
         try:
             message, client_address = server_socket.recvfrom(2048)
             message = str(client_address[0]) + ':' + str(client_address[1])
             server_socket.sendto(message.encode(), client_address)
-            logg(FILE_, r, 'sending a message to a client')
+            logg(file_, r, 'sending a message to a client')
         except KeyboardInterrupt:
-            logg(FILE_, r, 'the server is shutting down')
+            logg(file_, r, 'the server is shutting down')
             sys.exit(0)
 
 
 def udp_client(name_server, port_server, r):
     """Starting the client in UDP mode."""
     client_socket = socket(AF_INET, SOCK_DGRAM)
-    logg(FILE_, r, 'opening a socket with a protocol u')
+    logg(file_, r, 'opening a socket with a protocol u')
     message = 'hello'
     client_socket.sendto(message.encode(), (name_server, int(port_server)))
-    logg(FILE_, r, 'sending a message to a server')
-    modified_message, server_address = client_socket.recvfrom(2048)
-    logg(FILE_, r, 'Receiving a message')
+    logg(file_, r, 'sending a message to a server')
+    modified_message, _ = client_socket.recvfrom(2048)
+    logg(file_, r, 'Receiving a message')
     print(modified_message.decode())
     client_socket.close()
-    logg(FILE_, r, 'Closing the socket')
+    logg(file_, r, 'Closing the socket')
 
 
 def tcp_client(name_server, port_server, r):
     """Starting the client in TSP mode."""
     client_socket = socket(AF_INET, SOCK_STREAM)
-    logg(FILE_, r, 'opening a socket with a protocol t')
+    logg(file_, r, 'opening a socket with a protocol t')
     client_socket.connect((name_server, int(port_server)))
     sentence = 'hello'
     client_socket.send(sentence.encode())
-    logg(FILE_, r, 'sending a message to a server')
+    logg(file_, r, 'sending a message to a server')
     modified_sentence = client_socket.recv(1024)
-    logg(FILE_, r, 'Receiving a message')
+    logg(file_, r, 'Receiving a message')
     print('From Server:', modified_sentence.decode())
     client_socket.close()
-    logg(FILE_, r, 'Closing the socket')
+    logg(file_, r, 'Closing the socket')
 
 
 def tcp_server(port_server, r):
     """Starting the server in TSP mode."""
     server_socket = socket(AF_INET, SOCK_STREAM)
-    logg(FILE_, r, 'opening a socket with a protocol t')
+    logg(file_, r, 'opening a socket with a protocol t')
     server_socket.bind(('', int(port_server)))
     server_socket.listen(1)
     while 1:
@@ -84,44 +83,45 @@ def tcp_server(port_server, r):
             connection_socket, addr = server_socket.accept()
             capitalized_sentence = str(addr[0]) + ':' + str(addr[1])
             connection_socket.send(capitalized_sentence.encode())
-            logg(FILE_, r, 'sending a message to a client')
+            logg(file_, r, 'sending a message to a client')
             connection_socket.close()
         except KeyboardInterrupt:
-            logg(FILE_, r, 'the server is shutting down')
+            logg(file_, r, 'the server is shutting down')
             sys.exit(0)
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument("sN", type=str)
-PARSER.add_argument("sP", type=str)
-PARSER.add_argument("-s", action="store_true")
-COMMAND_GROUP = PARSER.add_mutually_exclusive_group()
-COMMAND_GROUP.add_argument('-u', help='TCP Protocol', action='store_true')
-COMMAND_GROUP.add_argument('-t', help='UDP Protocol', action='store_true')
-PARSER.add_argument("-o", action="store_true")
-PARSER_1 = create_parser_argv2()
-NAMESPACE = PARSER_1.parse_args(sys.argv[1:])
-ARGS = PARSER.parse_args()
-
-FILE_, SERVER_NAME, SERVERPORT = 'aaa', ARGS.sN, ARGS.sP
-CLIENT, PROTOCOL, READ = '-c', '-t', '-o'
-if ARGS.s:
-    CLIENT = '-s'
-if ARGS.u:
-    PROTOCOL = '-u'
-FILE_ = str(NAMESPACE.f)
-if FILE_ == "True":
-    READ = '-o'
-    logging.basicConfig(level=logging.DEBUG)
-if FILE_ != "True":
-    READ = '-f'
-    FILE_ = NAMESPACE.f
-    logging.basicConfig(filename=FILE_, level=logging.INFO)
-if PROTOCOL == '-u':
-    if CLIENT == '-s':
-        udp_server(SERVERPORT, READ)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("sN", type=str)
+    parser.add_argument("sP", type=str)
+    parser.add_argument("-s", action="store_true")
+    command_group = parser.add_mutually_exclusive_group()
+    command_group.add_argument('-u', help='TCP Protocol', action='store_true')
+    command_group.add_argument('-t', help='UDP Protocol', action='store_true')
+    parser.add_argument("-o", action="store_true")
+    parser_1 = create_parser_argv2()
+    namespace = parser_1.parse_args(sys.argv[1:])
+    args = parser.parse_args()
+    
+    file_, server_name, serverport = 'aaa', args.sN, args.sP
+    client, protocol, read = '-c', '-t', '-o'
+    if args.s:
+        client = '-s'
+    if args.u:
+        protocol = '-u'
+    file_ = str(namespace.f)
+    if file_ == "True":
+        read = '-o'
+        logging.basicConfig(level=logging.DEBUG)
+    if file_ != "True":
+        read = '-f'
+        file_ = namespace.f
+        logging.basicConfig(filename=file_, level=logging.INFO)
+    if protocol == '-u':
+        if client == '-s':
+            udp_server(serverport, read)
+        else:
+            udp_client(server_name, serverport, read)
     else:
-        udp_client(SERVER_NAME, SERVERPORT, READ)
-else:
-    if CLIENT != '-s':
-        tcp_client(SERVER_NAME, SERVERPORT, READ)
-    else:
-        tcp_server(SERVERPORT, READ)
+        if client != '-s':
+            tcp_client(server_name, serverport, read)
+        else:
+            tcp_server(serverport, read)
